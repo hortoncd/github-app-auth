d6b735fe34c71d669c38724db388629eaec6e1e9
Chris Horton <hortoncd@gmail.com>
2023-07-11 20:01:52 -0700

    remove deprecated app_installation_* methods

--- spec/github/app/auth_spec.rb
+++ spec/github/app/auth_spec.rb
@@ -8,12 +8,15 @@ RSpec.describe GitHub::App::Auth do
   it "provides a class that includes the module's methods" do
     auth = GitHub::App::Auth::AuthClass.new
     expect(auth).to be_kind_of(GitHub::App::Auth::AuthClass)
-    expect(auth.public_methods).to include(:app_installation_client)
     expected_methods = [
       :app_token,
       :app_client,
-      :app_installation_token,
-      :app_installation_client
+      :organization_installation_token,
+      :organization_installation_client,
+      :repository_installation_token,
+      :repository_installation_client,
+      :user_installation_token,
+      :user_installation_client
     ]
     expected_methods.each do |method|
       expect(auth.public_methods).to include(method)
